cd8fbb05841415527882afdba568e37b56141214
CodersAcademy006 <104912634+CodersAcademy006@users.noreply.github.com>
2024-08-17 00:20:09 -0700

    Add: Product array puzzle.java

---
+++ DSA/Product array puzzle.java
@@ -0,0 +1,27 @@
+class Solution {
+    public static long[] productExceptSelf(int nums[]) {
+        int n = nums.length;
+        long[] leftProducts = new long[n];
+        long[] rightProducts = new long[n];
+        long[] result = new long[n];
+        
+        // Calculate left products
+        leftProducts[0] = 1;
+        for (int i = 1; i < n; i++) {
+            leftProducts[i] = leftProducts[i - 1] * nums[i - 1];
+        }
+        
+        // Calculate right products
+        rightProducts[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            rightProducts[i] = rightProducts[i + 1] * nums[i + 1];
+        }
+        
+        // Calculate result using left and right products
+        for (int i = 0; i < n; i++) {
+            result[i] = leftProducts[i] * rightProducts[i];
+        }
+        
+        return result;
+    }
+}
