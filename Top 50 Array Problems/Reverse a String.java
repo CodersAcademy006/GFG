7a48b9057a4cd41685bf834da017e5bef6e86464
CodersAcademy006 <104912634+CodersAcademy006@users.noreply.github.com>
2024-08-20 22:10:18 -0700

    Add: Reverse a String.java

---
+++ Top 50 Array Problems/Reverse a String.java
@@ -0,0 +1,10 @@
+class Reverse
+{
+    // Complete the function
+    // str: input string
+    public static String reverseWord(String str)
+    {
+        // Using StringBuilder to reverse the string
+        return new StringBuilder(str).reverse().toString();
+    }
+}
